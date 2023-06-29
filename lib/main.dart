5815578e7141fe091b8b21bcc9a46f8a406c73bc
Dhina0508 <dhina0508@gmail.com>
2023-06-29 23:44:35 +0530

    29/6

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
         home: HomeScreen());
@@ -40,7 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<Widget> _screens = [
     // Content for Home tab
     Container(
-      color: Colors.yellow.shade100,
+      color: Colors.grey.shade300,
       alignment: Alignment.center,
       child: const Text(
         'Home',
@@ -92,7 +83,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ? BottomNavigationBar(
               currentIndex: _selectedIndex,
               unselectedItemColor: Colors.grey,
-              selectedItemColor: Colors.indigoAccent,
+              selectedItemColor: Colors.red,
               // called when one tab is selected
               onTap: (int index) {
                 setState(() {
